b99b848e450204ad6e1455cbb4ee485aa9909b7d
SteffenK <SteffenKrapf@gmail.com>
2016-05-23 03:06:21 +0200

    Added "avoidWalls" so robot wont hit walls anymore

--- src/robots/TestBot2.java
+++ src/robots/TestBot2.java
@@ -3,27 +3,25 @@ package robots;
 import static robocode.util.Utils.normalRelativeAngleDegrees;
 
 import java.awt.Color;
-
 import robocode.AdvancedRobot;
-import robocode.HitByBulletEvent;
+import robocode.HitWallEvent;
 import robocode.RobotDeathEvent;
 import robocode.Rules;
 import robocode.ScannedRobotEvent;
 import robocode.StatusEvent;
 
 public class TestBot2 extends AdvancedRobot {
-	private double MIN_BULLET_POWER = Rules.MIN_BULLET_POWER;
-	private double MAX_BULLET_POWER = Rules.MAX_BULLET_POWER;
 	private double lastRobotDistance;
 	private String lastRobotName;
 	private boolean lastRobotDied = false;
+	private double moveDirection = 1;
 
 	public void run() {
-
 		setBodyColor(Color.black);
 		setGunColor(Color.red);
 		setRadarColor(Color.black);
 		setBulletColor(Color.green);
+
 		setAdjustRadarForRobotTurn(true);
 		setAdjustGunForRobotTurn(true); // Keep turret still while moving
 		turnRadarRight(Double.POSITIVE_INFINITY);
@@ -32,20 +30,21 @@ public class TestBot2 extends AdvancedRobot {
 	public void onScannedRobot(ScannedRobotEvent e) {
 		double absBearing = e.getBearing() + getHeading();
 		double gunTurnAmt;
-		
-		//e.getHeading();
 
-		// Radar Stuff
-		// setTurnRadarLeft(getRadarTurnRemaining());// Lock on the radar
-		if (lastRobotName == null || lastRobotDied || e.getDistance() < lastRobotDistance
+		// check if there is an enemy closer than the last and hit him instead
+		if (lastRobotName == null || lastRobotDied
+				|| e.getDistance() < lastRobotDistance
 				|| e.getName().equals(lastRobotName)) {
 			// Gun Stuff
 			gunTurnAmt = normalRelativeAngleDegrees(absBearing
 					- getGunHeading());
+			// Adjust turn amount to compensate own movement
+			gunTurnAmt += (moveDirection * -1) * 5;
 			setTurnGunRight(gunTurnAmt); // turn gun
 			fireBullet(e);
 			lastRobotName = e.getName();
 			lastRobotDistance = e.getDistance();
+			absBearing = e.getBearing() + getHeading();
 		}
 	}
 
@@ -62,29 +61,81 @@ public class TestBot2 extends AdvancedRobot {
 
 	@Override
 	public void onRobotDeath(RobotDeathEvent event) {
-		// TODO Auto-generated method stub
 		super.onRobotDeath(event);
-		
-		if(lastRobotName == null || event.getName().equals(lastRobotName)){
+
+		// check if the last Robot you fired on Died
+		if (lastRobotName == null || event.getName().equals(lastRobotName)) {
 			lastRobotDied = true;
 		}
 	}
 
+	@Override
+	public void onHitWall(HitWallEvent event) {
+		super.onHitWall(event);
+
+		System.out.println(" X: " + getX() + " Y: " + getY());
+	}
+
 	public void onStatus(StatusEvent event) {
 		// TODO
+		avoidWalls();
+		setAhead(moveDirection * 20);
 	}
 
 	private double calcFirePowerLife(ScannedRobotEvent e) {
-		double firePower = 0.1;
+		double firePower = Rules.MIN_BULLET_POWER;
 		@SuppressWarnings("deprecation")
 		double life = e.getLife();
 
 		// raise bullet power till max or the damage is higher than life
 		while (Rules.getBulletDamage(firePower) < life
-				&& firePower <= MAX_BULLET_POWER) {
+				&& firePower <= Rules.MAX_BULLET_POWER) {
 			firePower += 0.1;
 		}
 
 		return firePower;
+	}
+
+	private boolean avoidWalls() {
+		double fieldWith = getBattleFieldWidth();
+		double fieldHeight = getBattleFieldHeight();
+		double avoidDistance = 120;
+		double turnDegree = 15;
+		
+//		System.out.println("stickX: "
+//				+ (getX() + Math.sin(Math.toRadians(this.getHeading())) * avoidDistance) + " stickY: "
+//				+ (getY() + Math.cos(Math.toRadians(this.getHeading())) * avoidDistance) + " heading: " + this.getHeading() 
+//				+ " X: " + getX() + " sinH: " + Math.sin(this.getHeading())
+//				+ " Y: " + getY() + " cosH: " + Math.cos(this.getHeading()));	
+		
+		if (getX() + Math.sin(Math.toRadians(this.getHeading())) * avoidDistance >= fieldWith - 36) {
+			//going to hit east wall
+			System.out.println("going to hit east wall---------------------------");
+			if(this.getHeading()> 0 && this.getHeading()< 90) turnLeft(turnDegree);
+			if(this.getHeading()> 90 && this.getHeading()< 180) turnRight(turnDegree);
+			return true;
+		}
+		if (getX() + Math.sin(Math.toRadians(this.getHeading())) * avoidDistance <= 18) {
+			//going to hit west wall
+			System.out.println("going to hit west wall---------------------------");
+			if(this.getHeading()> 270 && this.getHeading()< 360) turnRight(turnDegree);
+			if(this.getHeading()> 180 && this.getHeading()< 270) turnLeft(turnDegree);
+			return true;
+		}
+		if (getY() + Math.cos(Math.toRadians(this.getHeading())) * avoidDistance >= fieldHeight - 36) {
+			//going to hit north wall 
+			System.out.println("going to hit north wall---------------------------");
+			if(this.getHeading()> 270 && this.getHeading()< 360) turnLeft(turnDegree);
+			if(this.getHeading()> 0 && this.getHeading()< 90) turnRight(turnDegree);
+			return true;
+		}
+		if (getY() + Math.cos(Math.toRadians(this.getHeading())) * avoidDistance <= 18) {
+			//going to hit south wall 
+			System.out.println("going to hit south wall---------------------------");
+			if(this.getHeading()> 90 && this.getHeading()< 180) turnLeft(turnDegree);
+			if(this.getHeading()> 180 && this.getHeading()< 270) turnRight(turnDegree);
+			return true;
+		}
+		return false;
 	}
 }
