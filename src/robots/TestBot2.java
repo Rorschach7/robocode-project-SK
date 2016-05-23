1034313b6921f2d128c684a37f7452291f68f4cd
SteffenK <SteffenKrapf@gmail.com>
2016-05-23 03:08:26 +0200

    formatter

--- src/robots/TestBot2.java
+++ src/robots/TestBot2.java
@@ -101,39 +101,55 @@ public class TestBot2 extends AdvancedRobot {
 		double fieldHeight = getBattleFieldHeight();
 		double avoidDistance = 120;
 		double turnDegree = 15;
-		
-//		System.out.println("stickX: "
-//				+ (getX() + Math.sin(Math.toRadians(this.getHeading())) * avoidDistance) + " stickY: "
-//				+ (getY() + Math.cos(Math.toRadians(this.getHeading())) * avoidDistance) + " heading: " + this.getHeading() 
-//				+ " X: " + getX() + " sinH: " + Math.sin(this.getHeading())
-//				+ " Y: " + getY() + " cosH: " + Math.cos(this.getHeading()));	
-		
-		if (getX() + Math.sin(Math.toRadians(this.getHeading())) * avoidDistance >= fieldWith - 36) {
-			//going to hit east wall
-			System.out.println("going to hit east wall---------------------------");
-			if(this.getHeading()> 0 && this.getHeading()< 90) turnLeft(turnDegree);
-			if(this.getHeading()> 90 && this.getHeading()< 180) turnRight(turnDegree);
+
+		// TODO still bugs sometimes when heading to an edge
+
+		// System.out.println("stickX: "
+		// + (getX() + Math.sin(Math.toRadians(this.getHeading())) *
+		// avoidDistance) + " stickY: "
+		// + (getY() + Math.cos(Math.toRadians(this.getHeading())) *
+		// avoidDistance) + " heading: " + this.getHeading()
+		// + " X: " + getX() + " sinH: " + Math.sin(this.getHeading())
+		// + " Y: " + getY() + " cosH: " + Math.cos(this.getHeading()));
+
+		if (getX() + Math.sin(Math.toRadians(this.getHeading()))
+				* avoidDistance >= fieldWith - 36) {
+			// going to hit east wall
+			System.out.println("going to hit east wall-------------------");
+			if (this.getHeading() > 0 && this.getHeading() < 90)
+				turnLeft(turnDegree);
+			if (this.getHeading() > 90 && this.getHeading() < 180)
+				turnRight(turnDegree);
 			return true;
 		}
-		if (getX() + Math.sin(Math.toRadians(this.getHeading())) * avoidDistance <= 18) {
-			//going to hit west wall
-			System.out.println("going to hit west wall---------------------------");
-			if(this.getHeading()> 270 && this.getHeading()< 360) turnRight(turnDegree);
-			if(this.getHeading()> 180 && this.getHeading()< 270) turnLeft(turnDegree);
+		if (getX() + Math.sin(Math.toRadians(this.getHeading()))
+				* avoidDistance <= 18) {
+			// going to hit west wall
+			System.out.println("going to hit west wall-------------------");
+			if (this.getHeading() > 270 && this.getHeading() < 360)
+				turnRight(turnDegree);
+			if (this.getHeading() > 180 && this.getHeading() < 270)
+				turnLeft(turnDegree);
 			return true;
 		}
-		if (getY() + Math.cos(Math.toRadians(this.getHeading())) * avoidDistance >= fieldHeight - 36) {
-			//going to hit north wall 
-			System.out.println("going to hit north wall---------------------------");
-			if(this.getHeading()> 270 && this.getHeading()< 360) turnLeft(turnDegree);
-			if(this.getHeading()> 0 && this.getHeading()< 90) turnRight(turnDegree);
+		if (getY() + Math.cos(Math.toRadians(this.getHeading()))
+				* avoidDistance >= fieldHeight - 36) {
+			// going to hit north wall
+			System.out.println("going to hit north wall-------------------");
+			if (this.getHeading() > 270 && this.getHeading() < 360)
+				turnLeft(turnDegree);
+			if (this.getHeading() > 0 && this.getHeading() < 90)
+				turnRight(turnDegree);
 			return true;
 		}
-		if (getY() + Math.cos(Math.toRadians(this.getHeading())) * avoidDistance <= 18) {
-			//going to hit south wall 
-			System.out.println("going to hit south wall---------------------------");
-			if(this.getHeading()> 90 && this.getHeading()< 180) turnLeft(turnDegree);
-			if(this.getHeading()> 180 && this.getHeading()< 270) turnRight(turnDegree);
+		if (getY() + Math.cos(Math.toRadians(this.getHeading()))
+				* avoidDistance <= 18) {
+			// going to hit south wall
+			System.out.println("going to hit south wall-------------------");
+			if (this.getHeading() > 90 && this.getHeading() < 180)
+				turnLeft(turnDegree);
+			if (this.getHeading() > 180 && this.getHeading() < 270)
+				turnRight(turnDegree);
 			return true;
 		}
 		return false;
