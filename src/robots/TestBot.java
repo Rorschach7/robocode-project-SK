3318b4e81eade5c532eddeffa083ec14bcdd0cd9
Kevin Lichtenberg <kevlicht@gmail.com>
2016-06-06 16:11:20 +0200

    Improved Linear Targeting

--- src/robots/TestBot.java
+++ src/robots/TestBot.java
@@ -456,33 +456,49 @@ public class TestBot extends TeamRobot {
 		// Linear Targeting
 		// TODO: consider walls
 		if(fireMode == FireMode.LinearTargeting) {			
-			double absBearing = enemy.getBearing() + getHeading();
-			double gunTurnAmt;
 			
-			// TODO: Skip linear targeting if enemy is too close
-			// Calculate enemie's lateral velocity
-			double latVel = enemy.getVelocity()
-					* Math.sin(enemy.getHeadingRadians()
-							- Math.toRadians(absBearing));
-			
-			double bulletSpeed = 20 - 3 * (400 / target.getInfo().getDistance());
-			
-			gunTurnAmt = normalRelativeAngleDegrees(absBearing - getGunHeading()
-					+ ((latVel / bulletSpeed) * 57.3));
-			
-			setTurnGunRight(gunTurnAmt); // Turn gun
-			
-			if (target.getInfo().getDistance() < 600
-					&& getEnergy() > EnergyThreshold) {
-				if (Math.abs(getGunTurnRemaining()) <= 5) { // Don't shoot before
-					// gun is adjusted
-					setFire(400 / target.getInfo().getDistance());
-					// System.out.println("FIRE");
-				}
-			}
+			double power = Math.min(3, Math.max(.1, 400 / target.getInfo().getDistance()));
+		    final double ROBOT_WIDTH = 16,ROBOT_HEIGHT = 16;
+		    // Variables prefixed with e- refer to enemy, b- refer to bullet and r- refer to robot
+		    final double eAbsBearing = getHeadingRadians() + target.getInfo().getBearingRadians();
+		    final double rX = getX(), rY = getY(),
+		        bV = Rules.getBulletSpeed(power);
+		    final double eX = rX + target.getInfo().getDistance()*Math.sin(eAbsBearing),
+		        eY = rY + target.getInfo().getDistance()*Math.cos(eAbsBearing),
+		        eV = target.getInfo().getVelocity(),
+		        eHd = target.getInfo().getHeadingRadians();
+		    // These constants make calculating the quadratic coefficients below easier
+		    final double A = (eX - rX)/bV;
+		    final double B = eV/bV*Math.sin(eHd);
+		    final double C = (eY - rY)/bV;
+		    final double D = eV/bV*Math.cos(eHd);
+		    // Quadratic coefficients: a*(1/t)^2 + b*(1/t) + c = 0
+		    final double a = A*A + C*C;
+		    final double b = 2*(A*B + C*D);
+		    final double c = (B*B + D*D - 1);
+		    final double discrim = b*b - 4*a*c;
+		    if (discrim >= 0) {
+		        // Reciprocal of quadratic formula
+		        final double t1 = 2*a/(-b - Math.sqrt(discrim));
+		        final double t2 = 2*a/(-b + Math.sqrt(discrim));
+		        final double t = Math.min(t1, t2) >= 0 ? Math.min(t1, t2) : Math.max(t1, t2);
+		        // Assume enemy stops at walls
+		        final double endX = limit(
+		            eX + eV*t*Math.sin(eHd),
+		            ROBOT_WIDTH/2, getBattleFieldWidth() - ROBOT_WIDTH/2);
+		        final double endY = limit(
+		            eY + eV*t*Math.cos(eHd),
+		            ROBOT_HEIGHT/2, getBattleFieldHeight() - ROBOT_HEIGHT/2);
+		        setTurnGunRightRadians(robocode.util.Utils.normalRelativeAngle(
+		            Math.atan2(endX - rX, endY - rY)
+		            - getGunHeadingRadians()));
+		        setFire(power);
+		    }			
 		}
 		
-		if(fireMode == FireMode.GuessFactor) {				
+		if(fireMode == FireMode.GuessFactor) {
+			
+			// Guess Targeting
 			double absBearing = enemy.getBearingRadians() + getHeadingRadians();
 			double power = Math.min(3, Math.max(.1, 400 / target.getInfo().getDistance()));
 			
@@ -517,6 +533,10 @@ public class TestBot extends TeamRobot {
 			 // End of guess shoting 			
 		}
 		
+	}
+	
+	private double limit(double value, double min, double max) {
+	    return Math.min(max, Math.max(min, value));
 	}
 
 	/**
@@ -741,8 +761,7 @@ public class TestBot extends TeamRobot {
 		double ex = getX() + Math.sin(absBearing) * e.getDistance();
 		double ey = getY() + Math.cos(absBearing) * e.getDistance();	
 		
-		// Let's process the waves now:
-		EnemyBot enemyBot = findBotByName(e.getName());
+		// Let's process the waves now:		
 		for (int i = 0; i < waves.size(); i++) {
 			WaveBullet currentWave = (WaveBullet) waves.get(i);
 			if (currentWave.checkHit(ex, ey, getTime())) {
@@ -769,7 +788,9 @@ public class TestBot extends TeamRobot {
 	
 	private void chooseFireMode() {
 		
-		fireMode = FireMode.GuessFactor;
+		// TODO: 
+		
+		fireMode = FireMode.LinearTargeting;
 		
 	}
 	
